2a99fe74ec34051011b13cd74fb1c209955927aa
Joe Robinson <robinson.jo@husky.neu.edu>
2020-01-04 18:01:15 -0500

    datatable filename change

--- code/tests/test_io.py
+++ code/tests/test_io.py
@@ -11,7 +11,7 @@ DATADIR = pathlib.Path(__file__).parent.parent.parent.joinpath('data')
 
 def test_bias_dataframe():
     "Make sure the data is loaded properly"
-    fname = DATADIR.joinpath('final_datatable_all.pkl')
+    fname = DATADIR.joinpath('bfw-datatable.pkl')
     data = load_bfw_datatable(fname)
     assert len(data) == 923898
     npt.assert_allclose(data['score'].mean(), 0.5864211374778945)
