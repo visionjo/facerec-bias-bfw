fe3dd7e13052f29a411016831c70e64297e77362
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-01-18 15:11:04 -0500

    Determining accuracy across 5-folds.
    Single (global) threshold is used.

--- code/experiments/five-fold-evaluation.py
+++ code/experiments/five-fold-evaluation.py
@@ -1,41 +1,34 @@
-import pandas as pd
-import glob
 import numpy as np
 
 from facebias.utils import add_package_path
 
 add_package_path()
-
-
-from facebias.ioutils import (
-    load_bfw_datatable,
-    save_bfw_datatable,
-    load_features_from_image_list,
-)
-
+import facebias.utils as fbutils
+from facebias.io import load_bfw_datatable
 
 dir_data = "../../data/bfw-data/"
-dir_features = f"{dir_data}features/senet50/"
 f_datatable = f"{dir_data}bfw-datatable.pkl"
 
-global_threshold = 0.28
-
-data = load_bfw_datatable(f_datatable)
+thresholds = np.linspace(0.2, 0.9, 1000)
+data = load_bfw_datatable(f_datatable,
+                          cols=['p1', 'p2', 'score', 'label', 'fold',
+                                'senet50'])
 data["score"] = data["senet50"]
+del data['senet50']
 
-# create ali_images list of all faces (i.e., unique set)
-li_images = list(np.unique(data.p1.to_list() + data.p2.to_list()))
+# reorder for find_best_threshold(s
+data = data[['p1', 'p2', 'score', 'label', 'fold']]
 
-# read features as a dictionary, with keys set as the filepath of the image with values set as the face encodings
-features = load_features_from_image_list(li_images, dir_features, ext_feat="npy")
+global_threshold, _ = fbutils.find_best_threshold(thresholds, data)
 
 running_scores = []
 data["y1"] = (data["score"] > global_threshold).values.astype(int)
 for k in range(1, 1 + data.fold.max()):
+    # for each fold
     df_test = data.loc[data.fold == k]
     y_pred = df_test["y1"]
     y_true = df_test["label"].values.astype(int)
-    running_scores.append(sum(y_pred == y_true) / len(y_pred))
+    running_scores.append(sum((y_pred == y_true).astype(int)) / len(y_pred))
     print(running_scores[-1])
 print(np.mean(running_scores))
 data["iscorrect"] = data.y1 == data.label
