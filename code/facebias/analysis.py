c1610716fd44e555ae097f77cb1ce4e5de23f1d9
Suchan Vivatsethachai <suchanv@mit.edu>
2020-01-28 16:25:36 -0500

    add documentations, parser, and image pair and image list cleaning script

--- code/facebias/analysis.py
+++ code/facebias/analysis.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import pickle as pk
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import matplotlib.style as style
@@ -20,13 +21,27 @@ from facebias.preprocessing import get_attribute_gender_ethnicity, load_image_pa
 
 
 def violin_plot(data, save_figure_path=None):
+    """
+    Plot a violin plot of the distribution of the cosine similarity score of Imposter pairs (different people) and
+    Genuine pair (same people) the plots are separated by ethnicity-gender attribute of the first person of each pair
+    The final plot is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:               pandas.DataFrame that contains column 'p1', 'p2', 'a1', 'a2', 'score', and 'label'
+        'p1' and 'p2' are the pair of images. 'a1' and 'a2' are the abbreviated attribute of 'p1' and 'p2' respectively.
+        'score' is the cosine similarity score between 'p1' and 'p2', 'label' is a binary indicating whether 'p1' and
+        'p2' are the same person
+    save_figure_path:   path to save the resulting violin plot. will not save is the value is None
+
+    """
     fontsize = 12
     new_labels = ["Imposter", "Genuine"]
     palette = {new_labels[0]: "orange", new_labels[1]: "lightblue"}
 
-    data["Tag"] = data.label
-    data.loc[data.label == 0, "Tag"] = new_labels[0]
-    data.loc[data.label == 1, "Tag"] = new_labels[1]
+    data["Tag"] = data["label"]
+    data.loc[data["label"] == 0, "Tag"] = new_labels[0]
+    data.loc[data["label"] == 1, "Tag"] = new_labels[1]
 
     fig, ax = plt.subplots(1, 1, figsize=(13, 7))
     sns.violinplot(x="a1", y="score", hue="Tag", data=data, linewidth=1.25, dodge=True,
@@ -43,6 +58,21 @@ def violin_plot(data, save_figure_path=None):
 
 
 def det_plot(data, group_by, plot_title, save_figure_path=None):
+    """
+    Plot Detection Error Tradeoff (DET) curves. Each curve use data separated by column 'group_by' in 'data'.
+    Each DET curve is created by varying the threshold of the cosine similarity score which locates in 'score' column
+    of 'data'. The final plot has title 'plot_title' and is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:               pandas.DataFrame that contains column 'p1', 'p2', 'score', and 'label'. 'p1' and 'p2' are the
+        pair of images. 'score' is the cosine similarity score between 'p1' and 'p2'
+        'label' is a binary indicating whether 'p1' and 'p2' are the same person
+    group_by:           the column in 'data' that we want to use as a separator
+    plot_title:         the title of the plot
+    save_figure_path:   path to save the resulting det plot. will not save is the value is None
+
+    """
     subgroups = data.groupby(group_by)
     li_subgroups = subgroups.groups
 
@@ -71,6 +101,16 @@ def det_plot(data, group_by, plot_title, save_figure_path=None):
 
 
 def plot_confusion_matrix(df, save_figure_path=None):
+    """
+    Using the values from 'df', visualize confusion matrix by varying color intensity of each cell based on its value.
+    The resulting plot is saved to 'save_figure_path'
+
+    paramters
+    ---------
+    data:               pandas.DataFrame that contains values of all cells in the confusion matrix we want to plot
+    save_figure_path:   path to save the resulting confusion matrix plot. will not save is the value is None
+
+    """
     # plot confusion matrix in heatmap format
     fig, ax = plt.subplots(figsize=(9, 9))
 
@@ -100,6 +140,16 @@ def plot_confusion_matrix(df, save_figure_path=None):
 
 
 def confusion_matrix(image_list_path, embedding_dir_path, save_figure_path=None):
+    """
+    TODO
+
+    Parameters
+    ----------
+    image_list_path:        path to the file that contains list of images of interest
+    embedding_dir_path:     path to the root directory that contains all the embeddings.
+    save_figure_path:       path to save the resulting confusion matrix plot. will not save is the value is None
+
+    """
     data = pd.read_csv(image_list_path)
     image_list = data["path"].to_list()
     feature = load_features_from_image_list(image_list, embedding_dir_path, ext_feat="npy")
@@ -129,6 +179,32 @@ def confusion_matrix(image_list_path, embedding_dir_path, save_figure_path=None)
 
 def create_bias_analysis_plots(image_pair_path, image_list_path, embedding_dir_path, processed_data=None,
                                save_processed_data=None, save_figure_dir="results"):
+    """
+    Using image pairs from 'image_pair_path', plot the following two plots
+
+    Violin plot - the distribution of the cosine similarity score of Imposter pairs (different people) and Genuine pair
+        (same people) the plots are separated by ethnicity-gender attribute of the first person of each pair
+    DET plots - Detection Error Tradeoff curves. Each DET curve is created by varying the threshold of the cosine
+        similarity score between each image pair. Curves will be separated by three methods
+            i)      by gender
+            ii)     by ethnicity
+            iii)    by ethnicity-gender
+
+    Using the list of image from 'image_list_path'
+
+
+    Parameters
+    ----------
+    image_pair_path:        path to the file that contain all image pairs of interest
+    image_list_path:        path to the file that contains list of images of interest
+    embedding_dir_path:     path to the root directory that contains all the embeddings. in the root directories must
+        exist subdirectories with name {ethnicity}_{gender}s, each of which contain person id subdirectories
+    processed_data:         path to the saved processed dataframe that contain attributes, person unique id, and scores
+    save_processed_data:    path to save intermediate processed data (with attributes, person unique id, and scores).
+        will not save if the value is None
+    save_figure_dir:        path to save the resulting figures
+
+    """
     if processed_data is not None:
         with open(processed_data, "rb") as f:
             data_pair_df = pk.load(f)
@@ -146,3 +222,90 @@ def create_bias_analysis_plots(image_pair_path, image_list_path, embedding_dir_p
     det_plot(data_pair_df, "g1", "DET Curve Per Gender", join(save_figure_dir, "det_gender.png"))
     det_plot(data_pair_df, "e1", "DET Curve Per Ethnicity", join(save_figure_dir, "det_ethnicity.png"))
     confusion_matrix(image_list_path, embedding_dir_path, join(save_figure_dir, "confusion_matrix.png"))
+
+
+def replace_ext(path):
+    if ".jpg" in path:
+        path = path.replace(".jpg", ".npy")
+    elif ".png" in path:
+        path = path.replace(".png", ".npy")
+    return path
+
+
+def check_exist(rel_path):
+    return os.path.exists(os.path.join(embedding_dir_path, replace_ext(rel_path)))
+
+
+def clean_image_pair_and_image_list_csv(image_pair_path, image_list_path):
+    """
+    TODO
+
+    parameters
+    ----------
+    image_pair_path:
+    image_list_path:
+
+    returns
+    -------
+    image_pair_path
+    image_list_path
+
+    """
+    # clean image pair csv
+    image_pair = pd.read_csv(image_pair_path)
+    old_nrow = image_pair.shape[0]
+    image_pair = image_pair[image_pair["p1"].map(check_exist) & image_pair["p2"].map(check_exist)]
+    new_nrow = image_pair.shape[0]
+    print(f"For image pair csv, {old_nrow - new_nrow} rows out of {old_nrow} rows has been deleted"
+          f"({100 * (1 - new_nrow / old_nrow):.2f}% of all rows)")
+    new_filename = "updated_" + os.path.basename(image_pair_path)
+    image_pair_path = os.path.join(os.pardir(image_pair_path), new_filename)
+    image_pair.to_csv(image_pair_path)
+
+    # clean image list csv
+    image_list = pd.read_csv(image_list_path)
+    old_nrow = image_list.shape[0]
+    image_list = image_list[image_list["path"].map(check_exist)]
+    new_nrow = image_list.shape[0]
+    print(f"For image list csv, {old_nrow - new_nrow} rows out of {old_nrow} rows has been deleted"
+          f"({100 * (1 - new_nrow / old_nrow):.2f}% of all rows)")
+    new_filename = "updated_" + os.path.basename(image_list_path)
+    image_list_path = os.path.join(os.pardir(image_list_path), new_filename)
+    image_list.to_csv(image_list_path)
+
+    return image_pair_path, image_list_path
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="TODO")
+    parser.add_argument("-p", "--image_pair_path", type=str, action="store", required=True,
+                        help="path to the file that contain all image pairs of interest")
+    parser.add_argument("-l", "--image_list_path", type=str, action="store", required=True,
+                        help="path to the file that contains list of images of interest")
+    parser.add_argument("-e", "--embedding_path", type=str, action="store", required=True,
+                        help="path to the root directory that contains all the embeddings")
+    parser.add_argument("-s", "--save_path", type=str, action="store", required=True,
+                        help="path to the root directory to save figures and intermediate result")
+    parser.add_argument("-d", "--processed_data", type=str, action="store",
+                        help="path to the saved processed dataframe that contain attributes and scores")
+    parser.add_argument("-c", "--clean_image_pair_list", type=bool, action="store_true",
+                        help=("specified if image pair and image list needs to be modified by deleting rows that"
+                              "contains images that we do not have face embedding for"))
+    args = parser.parse_args()
+    image_pair_path = args.image_pair_path
+    image_list_path = args.image_list_path
+    clean_image_pair_list = args.clean_image_pair_list
+
+    if clean_image_pair_list:
+        image_pair_path, image_list_path = clean_image_pair_and_image_list_csv(image_pair_path, image_list_path)
+
+    embedding_dir_path = args.embedding_path
+    save_figure_dir = os.path.join(args.save_path, "results")
+    processed_data = args.processed_data
+    if processed_data is not None:
+        save_processed_data = None
+    else:
+        save_processed_data = os.path.join(args.save_path, "bfw-datatable.pkl")
+    # run the whole pipeline
+    create_bias_analysis_plots(image_pair_path, image_list_path, embedding_dir_path, processed_data,
+                               save_processed_data, save_figure_dir)
