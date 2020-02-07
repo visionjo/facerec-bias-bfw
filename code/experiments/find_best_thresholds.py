7e5222c28e4531c900f2106b170d6a583e8396be
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-02-07 18:53:23 -0500

    Updated find_best_thresholds.py

--- code/experiments/find_best_thresholds.py
+++ code/experiments/find_best_thresholds.py
@@ -1,74 +1,138 @@
+"""
+Find the optimal thresholds per subgroup
+"""
 import numpy as np
 import pandas as pd
-from facebias.utils import add_package_path
 
-add_package_path()
+from facebias.iotools import load_bfw_datatable
+from facebias.utils import find_best_threshold
 
-dir_data = "../../data/bfw-data/"
-f_datatable = f"{dir_data}bfw-v0.1.5-datatable.pkl"
+bfw_version = "0.1.5"
+dir_data = "../../data/bfw/"
 
-data = pd.read_pickle(f_datatable)
-data["score"] = data["senet50"]
+dir_features = f"{dir_data}features/sphereface/"
+dir_meta = f"{dir_data}meta/"
+f_datatable = f"{dir_meta}bfw-v{bfw_version}-datatable.pkl"
+f_threshold = f"{dir_meta}thresholds.pkl"
 
-thresholds = np.linspace(0.2, 0.9, 5000)
+thresholds_arr = np.linspace(0.18, 0.4, 5000)
+global_threshold = []
+data = load_bfw_datatable(f_datatable, default_score_col="sphereface")
+
+data["yp0"] = 0
+
+folds = data.fold.unique()
+for fold in folds:
+    ## Determine global thresholds and scores per fold
+    test_ids = data.fold == fold
+    threshold, score = find_best_threshold(
+        thresholds_arr, data.loc[test_ids == False, ["label", "score"]]
+    )
+    print(f"Fold {fold}: with t_g={threshold}, acc={score}")
+    data.loc[test_ids, "yp0"] = (data.loc[test_ids, "score"] >= threshold).astype(int)
+    global_threshold.append(threshold)
+
+data["iscorrect"] = (data["yp0"] == data["label"]).astype(int)
+
+print(
+    pd.DataFrame(
+        data.groupby("att1").sum()["iscorrect"]
+        / data.groupby("att1").count()["iscorrect"]
+    )
+)
+
+accuracy = (
+        data.groupby("att1").sum()["iscorrect"] / data.groupby("att1").count()["iscorrect"]
+).mean()
+std = (
+        data.groupby("att1").sum()["iscorrect"] / data.groupby("att1").count()["iscorrect"]
+).std()
+
+print(f"Accuracy:{accuracy}\nSTD:{std}")
+exit()
 
 subgroup_labels, gender_labels, ethnicity_labels = (
     data.a1.unique().astype(str),
     data.g1.unique().astype(str),
     data.e1.unique().astype(str),
 )
+
 subgroup_labels.sort()
 gender_labels.sort()
 ethnicity_labels.sort()
 
 best_thresholds = {}
-baseline_scores = {}
 best_scores = {}
-npairs = len(data)
+n_pairs = len(data)
 
 print("## All Subgroups ##")
 thr = 0.60
-data["optimal_threshold"] = None
-for atts in subgroup_labels:
-    ids = (data.a1 == atts) & (data.a2 == atts)
-    df = data.loc[ids]
+data["optimal_threshold"] = 0.0
+data["yp1"] = False
 
-    # bs, bt = f oimn
-    avg_scores = [((df.score > th) == df.label).mean() for th in thresholds]
-
-    max_ids = np.argmax(avg_scores)
-    best_scores[tuple(atts)] = avg_scores[max_ids]
-    best_thresholds[tuple(atts)] = thresholds[max_ids]
-    data.loc[ids, "optimal_threshold"] = thresholds[max_ids]
-
-    print(
-        "{0}-{1}: {2:.4f} with threshold of {3:.4f}".format(
-            atts[0], atts[1], avg_scores[max_ids], thresholds[max_ids]
+for att in subgroup_labels:
+    ## Determine global thresholds and scores per fold
+    best_thresholds_li = []
+    best_scores_li = []
+    df_subgroup = data.loc[data.a1 == att]
+    att_ids = (data.a1 == att) * (att == data.a2)
+    for fold in folds:
+        test_ids = df_subgroup.fold == fold
+        threshold, score = find_best_threshold(
+            thresholds_arr, df_subgroup.loc[test_ids == False, ["label", "score"]]
         )
+        print(f"Fold {fold}: with t_g={threshold}, acc={score}")
+        df_subgroup.loc[test_ids, "optimal_threshold"] = threshold
+
+        df_subgroup.loc[test_ids, "yp1"] = (df_subgroup["score"] >= threshold).astype(
+            int
+        )
+
+        best_thresholds_li.append(threshold)
+        best_scores_li.append(score)
+    data.loc[df_subgroup.index, "yp1"] = df_subgroup["yp1"]
+    data.loc[df_subgroup.index, "optimal_threshold"] = df_subgroup["optimal_threshold"]
+    best_thresholds[att] = best_thresholds_li
+    best_scores[att] = best_scores_li
+data["optimal_threshold"] = 0.6
+for att in subgroup_labels:
+    data.loc[(data.a1 == att) * (data.a2 == att), "optimal_threshold"] = np.mean(
+        best_thresholds[att]
     )
-    y_predict = df.score > thr
-    avg = (y_predict == df.label).mean()
-    print("{0:.4f} with threshold of {1:.4f}".format(avg, thr))
-    print()
-    baseline_scores[tuple(atts)] = avg
+
+data["yp1"] = data["optimal_threshold"] < data["score"]
+
+data["iscorrect1"] = (data["yp1"] == data["label"]).astype(int)
+print(
+    pd.DataFrame(
+        data.groupby("att1").sum()["iscorrect1"]
+        / data.groupby("att1").count()["iscorrect1"]
+    )
+)
+accuracy1 = (
+        data.groupby("att1").sum()["iscorrect1"]
+        / data.groupby("att1").count()["iscorrect1"]
+).mean()
+std1 = (
+        data.groupby("att1").sum()["iscorrect1"]
+        / data.groupby("att1").count()["iscorrect1"]
+).std()
+
+print(f"Accuracy:{accuracy1}\nSTD:{std1}")
+for att, scores in best_scores.items():
+    print(
+        f"{att}: {np.mean(best_thresholds[att])} +/- {np.std(best_thresholds[att])} at {np.mean(best_thresholds[att])}"
+        f" +/-  {np.std(best_thresholds[att])}"
+    )
+
+pd.to_pickle(data, "../../data/bfw/meta/datatable-w-threshold.pkl")
 
 meta = np.array(
     [
-        (k, th, s, sc)
-        for th, (k, s), sc in zip(
-            best_thresholds.values(), best_scores.items(), baseline_scores.values()
-        )
+        (k, np.mean(th), np.mean(s))
+        for th, (k, s) in zip(best_thresholds.values(), best_scores.items())
     ]
-).reshape(-1, 4)
+).reshape(-1, 3)
 print(meta)
-pd.to_pickle(meta, "../../data/interm/meta.pkl")
-data.to_pickle("../../data/interm/thresholds.pkl")
 
-for (k, th, s, sc) in meta:
-    print("{0}: {2:.4f} (th={1:.5f})\t {3:.5f} (baseline)".format(k, th, s, sc))
-
-# for m in meta:
-#     print("{0}:{1}\t{2:.4f}\t{3:.4f}".format(m))
-
-
-# print(best_thresholds)
+data.to_pickle(meta, "../../data/bfw/meta/optimal-thresholds.pkl")
