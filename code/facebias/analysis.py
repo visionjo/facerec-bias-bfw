f1c1594d2138dd4b119e2cc943733cedfcfb5277
Joe Robinson <robinson.jo@husky.neu.edu>
2020-02-27 20:54:37 -0500

    moved plotting functions to visualization.py

--- code/facebias/analysis.py
+++ code/facebias/analysis.py
@@ -4,359 +4,18 @@ import pickle as pk
 from os.path import join
 from pathlib import Path
 
-import matplotlib.colors as colors
-import matplotlib.pyplot as plt
-import matplotlib.style as style
-import matplotlib.ticker as mtick
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from facebias.iotools import load_features_from_image_list
-from facebias.metrics import calculate_det_curves
-from facebias.preprocessing import (
+from facebias.visualization import det_plot, overlapped_score_distribution, \
+    plot_confusion_matrix, violin_plot
+from iotools import load_features_from_image_list
+from preprocessing import (
     get_attribute_gender_ethnicity,
     load_image_pair_with_attribute_and_score,
 )
-from facebias.utils import replace_ext
-from facebias.visualization import draw_det_curve
-from matplotlib.lines import Line2D
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import LabelEncoder
-
-
-def violin_plot(data, save_figure_path=None):
-    """
-    Plot a violin plot of the distribution of the cosine similarity score of
-    impostor pairs (different people) and genuine pair (same people) the plots
-    are separated by ethnicity-gender attribute of the first person of each pair
-    The final plot is saved to 'save_figure_path'
-
-    Parameters
-    ----------
-    data:   pandas.DataFrame that contains column 'p1', 'p2', 'a1', 'a2',
-            'score', and 'label' 'p1' and 'p2' are the pair of images. 'a1' and
-            'a2' are the abbreviated attribute of 'p1' and 'p2' respectively.
-            'score' is the cosine similarity score between 'p1' and 'p2',
-            'label' is a binary indicating whether 'p1' and
-            'p2' are the same person
-    save_figure_path:   path to save the resulting violin plot. will not save is
-                        the value is None
-    """
-    fontsize = 12
-    new_labels = ["Imposter", "Genuine"]
-    palette = {new_labels[0]: "orange", new_labels[1]: "lightblue"}
-
-    data["Tag"] = data["label"]
-    data.loc[data["label"] == 0, "Tag"] = new_labels[0]
-    data.loc[data["label"] == 1, "Tag"] = new_labels[1]
-
-    fig, ax = plt.subplots(1, 1, figsize=(13, 7))
-    sns.violinplot(
-        x="a1",
-        y="score",
-        hue="Tag",
-        data=data,
-        linewidth=1.25,
-        dodge=True,
-        split=True,
-        palette=palette,
-        ax=ax,
-        scale_hue=True,
-        inner=None,
-    )
-    plt.xlabel("Subgroup", fontsize=fontsize)
-    plt.ylabel("Score", fontsize=fontsize)
-    ax.tick_params(axis="both", labelsize=fontsize)
-    plt.legend(loc="best", fontsize=fontsize)
-    plt.title(
-        "Score Distribution for Genuine and Imposter Pairs Across Subgroup",
-        fontsize=fontsize,
-    )
-
-    # save figure
-    if save_figure_path is not None:
-        plt.savefig(save_figure_path)
-
-
-def overlapped_score_distribution(data, log_scale=False, save_figure_path=None):
-    """
-    Plot the score distribution of the cosine similarity score of impostor pairs
-    (different people) and
-    Genuine pair (same people) the plots are separated by ethnicity-gender
-    attribute of the first person of each pair.
-    Curves of different ethnicity-gender attribute are distinguished by colors.
-    The final plot is saved to 'save_figure_path'
-
-    Parameters
-    ----------
-    data:               pandas.DataFrame that contains column 'p1', 'p2',
-                        'att1', 'att2', 'score', and 'label'
-                        'p1' and 'p2' are the pair of images.
-                        'att1' and 'att2' are the abbreviated attribute
-                        (ethnicity-gender) of 'p1' and 'p2' respectively.
-                        'score' is the cosine similarity score between 'p1' and
-                        'p2', 'label' is a binary indicating whether 'p1' and
-                        'p2' are the same person
-    log_scale:          boolean indicating whether to use log scale on y axis
-    save_figure_path:   path to save the resulting score distribution plot. will
-                        not save is the value is None
-    """
-    # set figure size
-    plt.figure(figsize=(20, 10))
-
-    # set color scheme and font size
-    att_to_color = {
-        "AM": "blue",
-        "AF": "orange",
-        "IM": "green",
-        "IF": "red",
-        "BM": "Purple",
-        "BF": "brown",
-        "WM": "hotpink",
-        "WF": "black",
-    }
-    fontsize = 14
-
-    # plot distribution for each ethnicity-gender attribute
-    for att in [f"{e}{g}" for e in ["A", "I", "B", "W"] for g in ["M", "F"]]:
-        data_att = data.loc[data["a1"] == att]
-
-        # plot intra score
-        sns.distplot(
-            data_att.loc[data_att["label"] == 1]["score"],
-            hist=False,
-            label=att,
-            color=att_to_color[att],
-        )
-        # plot inter score
-        sns.distplot(
-            data_att.loc[data_att["label"] == 0]["score"],
-            hist=False,
-            color=att_to_color[att],
-            kde_kws={"linestyle": "--"},
-        )
-
-    # set label and font sizes
-    plt.xlabel("Cosine Similarity Score", fontsize=fontsize)
-    plt.xticks(fontsize=fontsize)
-    plt.yticks(fontsize=fontsize)
-
-    # create legend
-    color_legend = plt.legend(fontsize=fontsize)
-    solid_line = Line2D([0], [0], color="black", linestyle="-")
-    dash_line = Line2D([0], [0], color="black", linestyle="--")
-    plt.legend([solid_line, dash_line], ["intra", "inter"], fontsize=fontsize,
-               loc=2)
-    plt.gca().add_artist(color_legend)
-
-    # handle log scale
-    if log_scale:
-        title = "Score Distribution Log Scale"
-        plt.semilogy()
-        plt.ylim([10 ** (-5), 10])
-    else:
-        title = "Score Distribution"
-
-    # set title
-    plt.title(title, fontsize=fontsize)
-
-    # save figure
-    if save_figure_path is not None:
-        plt.savefig(save_figure_path)
-
-
-def overlapped_score_distribution(data, log_scale=False, save_figure_path=None):
-    """
-    Plot the score distribution of the cosine similarity score of impostor pairs
-    (different people) and Genuine pair (same people) the plots are separated by
-    ethnicity-gender attribute of the first person of each pair. Curves of
-    different ethnicity-gender attribute are distinguished by colors.
-
-    The final plot is saved to 'save_figure_path'
-
-    Parameters
-    ----------
-    data:       pandas.DataFrame that contains column 'p1', 'p2', 'att1',
-                'att2', 'score', and 'label'
-                'p1' and 'p2' are the pair of images. 'att1' and 'att2' are the
-                abbreviated attribute (ethnicity-gender) of 'p1' and 'p2'
-                respectively. 'score' is the cosine similarity score between
-                'p1' and 'p2', 'label' is a binary indicating whether 'p1' and
-                'p2' are the same person
-    log_scale:          boolean indicating whether to use log scale on y axis
-    save_figure_path:   path to save the resulting score distribution plot. will
-                        not save is the value is None
-    """
-    # set figure size
-    plt.figure(figsize=(20, 10))
-
-    # set color scheme and font size
-    att_to_color = {
-        "AM": "blue",
-        "AF": "orange",
-        "IM": "green",
-        "IF": "red",
-        "BM": "Purple",
-        "BF": "brown",
-        "WM": "hotpink",
-        "WF": "black",
-    }
-    fontsize = 14
-
-    # plot distribution for each ethnicity-gender attribute
-    for att in [f"{e}{g}" for e in ["A", "I", "B", "W"] for g in ["M", "F"]]:
-        data_att = data.loc[data["a1"] == att]
-
-        # plot intra score
-        sns.distplot(
-            data_att.loc[data_att["label"] == 1]["score"],
-            hist=False,
-            label=att,
-            color=att_to_color[att],
-        )
-        # plot inter score
-        sns.distplot(
-            data_att.loc[data_att["label"] == 0]["score"],
-            hist=False,
-            color=att_to_color[att],
-            kde_kws={"linestyle": "--"},
-        )
-
-    # set label and font sizes
-    plt.xlabel("Cosine Similarity Score", fontsize=fontsize)
-    plt.xticks(fontsize=fontsize)
-    plt.yticks(fontsize=fontsize)
-
-    # create legend
-    color_legend = plt.legend(fontsize=fontsize)
-    solid_line = Line2D([0], [0], color="black", linestyle="-")
-    dash_line = Line2D([0], [0], color="black", linestyle="--")
-    plt.legend([solid_line, dash_line], ["intra", "inter"], fontsize=fontsize,
-               loc=2)
-    plt.gca().add_artist(color_legend)
-
-    # handle log scale
-    if log_scale:
-        title = "Score Distribution Log Scale"
-        plt.semilogy()
-        plt.ylim([10 ** (-5), 10])
-    else:
-        title = "Score Distribution"
-
-    # set title
-    plt.title(title, fontsize=fontsize)
-
-    # save figure
-    if save_figure_path is not None:
-        plt.savefig(save_figure_path)
-
-
-def det_plot(data, group_by, plot_title, save_figure_path=None):
-    """
-    Plot Detection Error Tradeoff (DET) curves. Each curve use data separated by
-    column 'group_by' in 'data'.
-
-    Each DET curve is created by varying the threshold of the cosine similarity
-    score which locates in 'score' column of 'data'. The final plot has title
-    'plot_title' and is saved to 'save_figure_path'
-
-    Parameters
-    ----------
-    data:               pandas.DataFrame that contains column 'p1', 'p2',
-                        'score', and 'label'. 'p1' and 'p2' are the
-                        pair of images. 'score' is the cosine similarity score
-                        between 'p1' and 'p2'
-                        'label' is a binary indicating whether 'p1' and 'p2'
-                        are the same person
-    group_by:           the column in 'data' that we want to use as a separator
-    plot_title:         the title of the plot
-    save_figure_path:   path to save the resulting det plot. will not save is
-                        the value is None
-    """
-    subgroups = data.groupby(group_by)
-    li_subgroups = subgroups.groups
-
-    fontsize = 12
-    fig, ax = plt.subplots(figsize=(8, 8), constrained_layout=True)
-    for subgroup in li_subgroups:
-        # for each subgroup
-        df_subgroup = subgroups.get_group(subgroup)
-        labels, scores = (
-            df_subgroup["label"].values.astype(int),
-            df_subgroup["score"].values,
-        )
-        fpr, fnr, thresholds = calculate_det_curves(labels, scores)
-        ax = draw_det_curve(
-            fpr, fnr, ax=ax, label=subgroup, fontsize=fontsize, title=plot_title
-        )
-
-    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter("%.e"))
-    plt.minorticks_off()
-    ax.set_ylabel("FNR (%)", fontsize=fontsize)
-    ax.set_xlabel("FPR", fontsize=fontsize)
-    plt.legend(fontsize=fontsize)
-    ax.set_xlim([1e-4, 1])
-    ax.set_ylim([0, 30])
-
-    ax.tick_params(axis="both", labelsize=fontsize)
-
-    # save figure
-    if save_figure_path is not None:
-        plt.savefig(save_figure_path)
-
-
-def plot_confusion_matrix(data, save_figure_path=None):
-    """
-    Using the values from 'df', visualize confusion matrix by varying color
-    intensity of each cell based on its value.
-
-    The resulting plot is saved to 'save_figure_path'
-
-    Parameters
-    ---------
-    data:               pandas.DataFrame that contains values of all cells in
-                        the confusion matrix we want to plot
-    save_figure_path:   path to save the resulting confusion matrix plot. will
-                        not save is the value is None
-    """
-    # plot confusion matrix in heatmap format
-    fig, ax = plt.subplots(figsize=(9, 9))
-
-    # set color scheme and style
-    cmap = colors.LinearSegmentedColormap.from_list(
-        "nameofcolormap", ["w", "b"], gamma=2.0
-    )
-    style.use("seaborn-paper")  # sets the size of the charts
-    sns.set_style({"xtick.bottom": True}, {"ytick.left": True})
-
-    # plot heatmap using seaborn
-    ax = sns.heatmap(
-        data,
-        annot=True,
-        linewidths=0.1,
-        square=True,
-        cmap=cmap,
-        cbar_kws={"shrink": 0.7, "ticks": [0.0, 2.5, 5.0, 7.5, 10.0, 12.5]},
-        linecolor="black",
-        ax=ax,
-        fmt=".2f",
-        annot_kws={"size": 14},
-        cbar=True,
-    )
-
-    # add the column names as labels, set fontsize and set title
-    fontsize = 14
-    ax.set_yticklabels(data.columns, rotation=0, fontsize=fontsize)
-    ax.set_xticklabels(data.columns, fontsize=fontsize)
-    ax.axhline(y=0, color="k", linewidth=2)
-    ax.axhline(y=data.shape[1], color="k", linewidth=2)
-    ax.axvline(x=0, color="k", linewidth=2)
-    ax.axvline(x=data.shape[0], color="k", linewidth=2)
-    ax.set_title("Rank 1 (%) Error", fontsize=fontsize)
-
-    # save figure
-    if save_figure_path is not None:
-        plt.savefig(save_figure_path)
+from utils import replace_ext
 
 
 def confusion_matrix(im_paths, dir_embeddings, save_figure_path=None):
@@ -381,8 +40,8 @@ def confusion_matrix(im_paths, dir_embeddings, save_figure_path=None):
                                             ext_feat="npy")
     data = get_attribute_gender_ethnicity(data, "path")
     data["id"] = (
-        data["path"].apply(lambda x:
-                           "/".join(x.split("/")[:-1])).astype("category")
+        data["path"].apply(lambda x: "/".join(x.split("/")[:-1])).astype(
+            "category")
     )
     score_matrix = cosine_similarity(
         data["path"].apply(lambda x: feature[x][0]).to_list()
@@ -534,8 +193,8 @@ def create_bias_analysis_plots(
         f"producing confusion matrix plot. result will be saved to "
         f"{confusion_matrix_path}"
     )
-    confusion_matrix(
-        im_paths, dir_embeddings, save_figure_path=confusion_matrix_path)
+    confusion_matrix(im_paths, dir_embeddings,
+                     save_figure_path=confusion_matrix_path)
 
 
 def clean_image_pair_and_image_list_csv(pair_paths, im_paths, dir_embeddings):
@@ -564,7 +223,7 @@ def clean_image_pair_and_image_list_csv(pair_paths, im_paths, dir_embeddings):
     old_nrow = image_pair.shape[0]
     image_pair = image_pair[
         image_pair["p1"].map(check_exist) & image_pair["p2"].map(check_exist)
-        ]
+    ]
     new_nrow = image_pair.shape[0]
     print(
         f"For image pair csv, {old_nrow - new_nrow} rows out of {old_nrow} rows"
--- code/facebias/visualization.py
+++ code/facebias/visualization.py
@@ -1,38 +1,96 @@
 """
 Functions for generating plots and visuals.
 """
-import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from matplotlib import colors as colors, pyplot as plt, style as style, \
+    ticker as mtick
+from matplotlib.lines import Line2D
 from matplotlib.ticker import ScalarFormatter
 from sklearn.metrics import auc, roc_curve
 
 
 def set_defaults(
-    font={
-        "font.family": "serif",
-        "font.serif": "Times New Roman",
-        "font.color": "darkred",
-        "font.weight": "normal",
-        "font.size": 16,
-    },
-    rc={"axes.facecolor": (0, 0, 0, 0)},
-    style="white",
-    gridstyle="whitegrid",
+        font={
+            "font.family": "serif",
+            "font.serif" : "Times New Roman",
+            "font.color" : "darkred",
+            "font.weight": "normal",
+            "font.size"  : 16,
+        },
+        rc={"axes.facecolor": (0, 0, 0, 0)},
+        style="white",
+        gridstyle="whitegrid",
 ):
     sns.set(style=style, rc=rc)
     sns.set_style(gridstyle, font)
 
 
+def box_plot(
+        data,
+        save_figure_path=None,
+        fontsize=12,
+        new_labels=("Imposter", "Genuine"),
+        figsize=(13, 7),
+):
+    """
+    Plot a violin plot of the distribution of the cosine similarity score of
+    impostor pairs (different people) and genuine pair (same people) the plots
+    are separated by ethnicity-gender attribute of the first person of each pair
+    The final plot is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:   pandas.DataFrame that contains column 'p1', 'p2', 'a1', 'a2',
+            'score', and 'label' 'p1' and 'p2' are the pair of images. 'a1' and
+            'a2' are the abbreviated attribute of 'p1' and 'p2' respectively.
+            'score' is the cosine similarity score between 'p1' and 'p2',
+            'label' is a binary indicating whether 'p1' and
+            'p2' are the same person
+    save_figure_path:   path to save the resulting violin plot. will not save is
+                        the value is None
+    """
+    palette = {new_labels[0]: "orange", new_labels[1]: "lightblue"}
+    data["Tag"] = data["label"]
+    data.loc[data["label"] == 0, "Tag"] = new_labels[0]
+    data.loc[data["label"] == 1, "Tag"] = new_labels[1]
+
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    sns.boxplot(
+        x="a1",
+        y="score",
+        hue="Tag",
+        data=data,
+        linewidth=1.25,
+        dodge=True,
+        notch=True,
+        palette=palette,
+        ax=ax,
+    )
+    plt.xlabel("Subgroup", fontsize=fontsize)
+    plt.ylabel("Score", fontsize=fontsize)
+    ax.tick_params(axis="both", labelsize=fontsize)
+    plt.legend(loc="best", fontsize=fontsize)
+    plt.title(
+        "Score Distribution for Genuine and Imposter Pairs Across Subgroup",
+        fontsize=fontsize,
+    )
+
+    plt.tight_layout()
+    # save figure
+    if save_figure_path is not None:
+        plt.savefig(save_figure_path, transparent=True)
+
+
 def draw_det_curve(
-    fpr,
-    fnr,
-    ax=None,
-    label=None,
-    set_axis_log_x=True,
-    set_axis_log_y=False,
-    scale=100,
-    title=None,
+        fpr,
+        fnr,
+        ax=None,
+        label=None,
+        set_axis_log_x=True,
+        set_axis_log_y=False,
+        scale=100,
+        title=None,
     label_x="FPR",
     label_y="FNR (%)",
     ticks_to_use_x=(1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e-0),
@@ -142,3 +200,251 @@ def generate_roc(
         plt.savefig(fpath)
 
     return ax
+
+
+def violin_plot(data, save_figure_path=None):
+    """
+    Plot a violin plot of the distribution of the cosine similarity score of
+    impostor pairs (different people) and genuine pair (same people) the plots
+    are separated by ethnicity-gender attribute of the first person of each pair
+    The final plot is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:   pandas.DataFrame that contains column 'p1', 'p2', 'a1', 'a2',
+            'score', and 'label' 'p1' and 'p2' are the pair of images. 'a1' and
+            'a2' are the abbreviated attribute of 'p1' and 'p2' respectively.
+            'score' is the cosine similarity score between 'p1' and 'p2',
+            'label' is a binary indicating whether 'p1' and
+            'p2' are the same person
+    save_figure_path:   path to save the resulting violin plot. will not save is
+                        the value is None
+    """
+    fontsize = 12
+    new_labels = ["Imposter", "Genuine"]
+    palette = {new_labels[0]: "orange", new_labels[1]: "lightblue"}
+
+    data["Tag"] = data["label"]
+    data.loc[data["label"] == 0, "Tag"] = new_labels[0]
+    data.loc[data["label"] == 1, "Tag"] = new_labels[1]
+
+    fig, ax = plt.subplots(1, 1, figsize=(13, 7))
+    sns.violinplot(
+        x="a1",
+        y="score",
+        hue="Tag",
+        data=data,
+        linewidth=1.25,
+        dodge=True,
+        split=True,
+        palette=palette,
+        ax=ax,
+        scale_hue=True,
+        inner=None,
+    )
+    plt.xlabel("Subgroup", fontsize=fontsize)
+    plt.ylabel("Score", fontsize=fontsize)
+    ax.tick_params(axis="both", labelsize=fontsize)
+    plt.legend(loc="best", fontsize=fontsize)
+    plt.title(
+        "Score Distribution for Genuine and Imposter Pairs Across Subgroup",
+        fontsize=fontsize,
+    )
+
+    # save figure
+    if save_figure_path is not None:
+        plt.savefig(save_figure_path)
+
+
+def overlapped_score_distribution(data, log_scale=False, save_figure_path=None):
+    """
+    Plot the score distribution of the cosine similarity score of impostor pairs
+    (different people) and Genuine pair (same people) the plots are separated by
+    ethnicity-gender attribute of the first person of each pair. Curves of
+    different ethnicity-gender attribute are distinguished by colors.
+
+    The final plot is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:       pandas.DataFrame that contains column 'p1', 'p2', 'att1',
+                'att2', 'score', and 'label'
+                'p1' and 'p2' are the pair of images. 'att1' and 'att2' are the
+                abbreviated attribute (ethnicity-gender) of 'p1' and 'p2'
+                respectively. 'score' is the cosine similarity score between
+                'p1' and 'p2', 'label' is a binary indicating whether 'p1' and
+                'p2' are the same person
+    log_scale:          boolean indicating whether to use log scale on y axis
+    save_figure_path:   path to save the resulting score distribution plot. will
+                        not save is the value is None
+    """
+    # set figure size
+    plt.figure(figsize=(20, 10))
+
+    # set color scheme and font size
+    att_to_color = {
+        "AM": "blue",
+        "AF": "orange",
+        "IM": "green",
+        "IF": "red",
+        "BM": "Purple",
+        "BF": "brown",
+        "WM": "hotpink",
+        "WF": "black",
+    }
+    fontsize = 14
+
+    # plot distribution for each ethnicity-gender attribute
+    for att in [f"{e}{g}" for e in ["A", "I", "B", "W"] for g in ["M", "F"]]:
+        data_att = data.loc[data["a1"] == att]
+
+        # plot intra score
+        sns.distplot(
+            data_att.loc[data_att["label"] == 1]["score"],
+            hist=False,
+            label=att,
+            color=att_to_color[att],
+        )
+        # plot inter score
+        sns.distplot(
+            data_att.loc[data_att["label"] == 0]["score"],
+            hist=False,
+            color=att_to_color[att],
+            kde_kws={"linestyle": "--"},
+        )
+
+    # set label and font sizes
+    plt.xlabel("Cosine Similarity Score", fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+
+    # create legend
+    color_legend = plt.legend(fontsize=fontsize)
+    solid_line = Line2D([0], [0], color="black", linestyle="-")
+    dash_line = Line2D([0], [0], color="black", linestyle="--")
+    plt.legend([solid_line, dash_line], ["intra", "inter"], fontsize=fontsize,
+               loc=2)
+    plt.gca().add_artist(color_legend)
+
+    # handle log scale
+    if log_scale:
+        title = "Score Distribution Log Scale"
+        plt.semilogy()
+        plt.ylim([10 ** (-5), 10])
+    else:
+        title = "Score Distribution"
+
+    # set title
+    plt.title(title, fontsize=fontsize)
+
+    # save figure
+    if save_figure_path is not None:
+        plt.savefig(save_figure_path)
+
+
+def det_plot(data, group_by, plot_title, save_figure_path=None):
+    """
+    Plot Detection Error Tradeoff (DET) curves. Each curve use data separated by
+    column 'group_by' in 'data'.
+
+    Each DET curve is created by varying the threshold of the cosine similarity
+    score which locates in 'score' column of 'data'. The final plot has title
+    'plot_title' and is saved to 'save_figure_path'
+
+    Parameters
+    ----------
+    data:               pandas.DataFrame that contains column 'p1', 'p2',
+                        'score', and 'label'. 'p1' and 'p2' are the
+                        pair of images. 'score' is the cosine similarity score
+                        between 'p1' and 'p2'
+                        'label' is a binary indicating whether 'p1' and 'p2'
+                        are the same person
+    group_by:           the column in 'data' that we want to use as a separator
+    plot_title:         the title of the plot
+    save_figure_path:   path to save the resulting det plot. will not save is
+                        the value is None
+    """
+    subgroups = data.groupby(group_by)
+    li_subgroups = subgroups.groups
+
+    fontsize = 12
+    fig, ax = plt.subplots(figsize=(8, 8), constrained_layout=True)
+    for subgroup in li_subgroups:
+        # for each subgroup
+        df_subgroup = subgroups.get_group(subgroup)
+        labels, scores = (
+            df_subgroup["label"].values.astype(int),
+            df_subgroup["score"].values,
+        )
+        fpr, fnr, thresholds = calculate_det_curves(labels, scores)
+        ax = draw_det_curve(
+            fpr, fnr, ax=ax, label=subgroup, fontsize=fontsize, title=plot_title
+        )
+
+    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter("%.e"))
+    plt.minorticks_off()
+    ax.set_ylabel("FNR (%)", fontsize=fontsize)
+    ax.set_xlabel("FPR", fontsize=fontsize)
+    plt.legend(fontsize=fontsize)
+    ax.set_xlim([1e-4, 1])
+    ax.set_ylim([0, 30])
+
+    ax.tick_params(axis="both", labelsize=fontsize)
+
+    # save figure
+    if save_figure_path is not None:
+        plt.savefig(save_figure_path)
+
+
+def plot_confusion_matrix(data, save_figure_path=None):
+    """
+    Using the values from 'df', visualize confusion matrix by varying color
+    intensity of each cell based on its value.
+
+    The resulting plot is saved to 'save_figure_path'
+
+    Parameters
+    ---------
+    data:               pandas.DataFrame that contains values of all cells in
+                        the confusion matrix we want to plot
+    save_figure_path:   path to save the resulting confusion matrix plot. will
+                        not save is the value is None
+    """
+    # plot confusion matrix in heatmap format
+    fig, ax = plt.subplots(figsize=(9, 9))
+
+    # set color scheme and style
+    cmap = colors.LinearSegmentedColormap.from_list(
+        "nameofcolormap", ["w", "b"], gamma=2.0
+    )
+    style.use("seaborn-paper")  # sets the size of the charts
+    sns.set_style({"xtick.bottom": True}, {"ytick.left": True})
+
+    # plot heatmap using seaborn
+    ax = sns.heatmap(
+        data,
+        annot=True,
+        linewidths=0.1,
+        square=True,
+        cmap=cmap,
+        cbar_kws={"shrink": 0.7, "ticks": [0.0, 2.5, 5.0, 7.5, 10.0, 12.5]},
+        linecolor="black",
+        ax=ax,
+        fmt=".2f",
+        annot_kws={"size": 14},
+        cbar=True,
+    )
+
+    # add the column names as labels, set fontsize and set title
+    fontsize = 14
+    ax.set_yticklabels(data.columns, rotation=0, fontsize=fontsize)
+    ax.set_xticklabels(data.columns, fontsize=fontsize)
+    ax.axhline(y=0, color="k", linewidth=2)
+    ax.axhline(y=data.shape[1], color="k", linewidth=2)
+    ax.axvline(x=0, color="k", linewidth=2)
+    ax.axvline(x=data.shape[0], color="k", linewidth=2)
+    ax.set_title("Rank 1 (%) Error", fontsize=fontsize)
+
+    # save figure
+    if save_figure_path is not None:
+        plt.savefig(save_figure_path)
