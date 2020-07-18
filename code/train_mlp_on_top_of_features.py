5033b2f1d5fe45b65a98dbf612801acb55727bc8
Paul Miller <paul@paulmillr.com>
2020-07-18 19:24:16 -0400

    refactor: modulated get_finetuned_mlp by adding to the facebias.models.mlp module; handle K-fold experiments-- save models, logs, and plot summary per fold.

--- code/train_mlp_on_top_of_features.py
+++ code/train_mlp_on_top_of_features.py
@@ -1,51 +1,27 @@
 import argparse
 import datetime
 import os
+import sys
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from facebias.iotools import split_bfw_features
 from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam, RMSprop
+
+dir_package = str(Path('./facebias').resolve())
+if dir_package not in sys.path:
+    print(f"adding {dir_package} to path")
+    sys.path.append(dir_package)
+else:
+    print(dir_package)
+
+from facebias.models.mlp import get_finetuned_mlp
+from facebias.iotools import split_bfw_features
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-def get_mlp_definition(input_shape, output_size=1, output_activation='sigmoid',
-                       optimizer=Adam(1e-4),  # "rmsprop",
-                       loss="binary_crossentropy", metrics=None):
-    """
-    Define MLP and compile loss on top. Default settings are for a binary classifier (i.e., gender recognition).
-    @param output_size: number of output predictions (i.e., number of classes or just 1 for binary)
-    @type output_size: int
-    @param output_activation:   activation function used to normalize the output
-    @param optimizer:   optimization schema
-    @param loss:    type of loss function
-    @param metrics:     metrics to optimize according to
-    @type metrics: list
-    @return: Keras model compiled with loss on top of MLP
-
-    """
-    if metrics is None:
-        metrics = ["accuracy"]
-    model = Sequential()
-    model.add(Flatten(input_shape=input_shape))
-    model.add(Dense(512, activation="relu"))
-    model.add(Dropout(0.5))
-    # model.add(Dense(512, activation="relu"))
-    # model.add(Dropout(0.5))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(0.5))
-
-    model.add(Dense(output_size, activation=output_activation))
-
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-    return model
+N_FOLDS = 5
 
 
 def plot_summary(N, filepath):
@@ -66,6 +42,7 @@ def plot_summary(N, filepath):
     plt.xlabel("Epoch #")
     plt.ylabel("Accuracy")
     plt.legend(loc="lower left")
+    Path(filepath).parent.mkdir(exist_ok=True, parents=True)
     plt.savefig(filepath)
 
 
@@ -84,11 +61,12 @@ parser.add_argument('-g', '--gender', action="store_true",
                     help='train gender classifier')
 parser.add_argument('-e', '--ethnicity', action="store_true",
                     help='train ethnicity classifier')
-parser.add_argument('-w', '--weights', type=str, default='train_models1',
+parser.add_argument('-w', '--weights', type=str, default='train_models',
                     help='path to dump trained weights')
 parser.add_argument('-o', '--optimizer', type=str, default='adam',
                     help='optimizer to train with (i.e., "adam" or not)')
 args = parser.parse_args()
+
 
 dir_features = str(Path(args.input).parent)
 f_meta = args.datatable
@@ -102,44 +80,55 @@ epochs = args.epochs
 batch_size = args.batch
 
 if args.gender:
-    train_ref, train_features, train_labels, val_ref, val_features, val_labels \
-        = split_bfw_features(f_meta, dir_features)
+    for val_fold in range(1, N_FOLDS + 1):
+        print(f"fold {val_fold}")
+        ref_tr, features_tr, labels_tr, ref_val, features_val, labels_val \
+            = split_bfw_features(f_meta, dir_features, val_fold=val_fold)
 
-    opt = Adam(lr=1e-4) if args.optimizer == 'adam' \
-        else RMSprop(0.0001, decay=1e-6)
-    model = get_mlp_definition(train_features.shape[1:], optimizer=opt)
-    tb_callback = TensorBoard(log_dir=f"{args.logs}/gender/{output_tag}")
+        opt = Adam(lr=1e-4) if args.optimizer == 'adam' \
+            else RMSprop(0.0001, decay=1e-6)
+        model = get_finetuned_mlp(features_tr.shape[1:], optimizer=opt)
+        dir_out = f"{args.logs}/gender/{output_tag}/{val_fold}"
+        tb_callback = TensorBoard(log_dir=dir_out)
 
-    model.fit(
-        train_features,
-        train_labels,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(val_features, val_labels),
-        callbacks=[tb_callback]
-    )
-    model.save_weights(str(path_weights / "gender_fc_model.h5"))
-    plot_summary(epochs, f"{args.logs}/gender/{output_tag}plot_summary.pdf")
+        model.fit(
+            features_tr,
+            labels_tr,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(features_val, labels_val),
+            callbacks=[tb_callback]
+        )
+        path_out = path_weights / str(val_fold) / "gender_fc_model.h5"
+        path_out.parent.mkdir(exist_ok=True, parents=True)
+        model.save_weights(str(path_out))
+        f_out = f"{args.logs}/gender/{output_tag}/{val_fold}/plot_summary"
+        plot_summary(epochs, f"{f_out}.pdf")
 
 if args.ethnicity:
-    train_ref, train_features, train_labels, val_ref, val_features, val_labels \
-        = split_bfw_features(f_meta, dir_features, 'ethnicity')
+    for val_fold in range(1, N_FOLDS + 1):
+        ref_tr, features_tr, labels_tr, ref_val, features_val, labels_val \
+            = split_bfw_features(f_meta, dir_features, 'ethnicity')
 
-    opt = Adam(lr=1e-4) if args.optimizer == 'adam' \
-        else RMSprop(0.0001, decay=1e-6)
-    model = get_mlp_definition(train_features.shape[1:], optimizer=opt,
-                               loss='sparse_categorical_crossentropy',
-                               output_activation='softmax', output_size=4)
+        opt = Adam(lr=1e-4) if args.optimizer == 'adam' \
+            else RMSprop(0.0001, decay=1e-6)
+        model = get_finetuned_mlp(features_tr.shape[1:], optimizer=opt,
+                                  loss='sparse_categorical_crossentropy',
+                                  output_activation='softmax', output_size=4)
+        dir_out = f"{args.logs}/ethnicity/{output_tag}/{val_fold}"
+        tb_callback = TensorBoard(log_dir=dir_out)
 
-    tb_callback = TensorBoard(log_dir=f"{args.logs}/ethnicity/{output_tag}")
+        model.fit(
+            features_tr,
+            labels_tr,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(features_val, labels_val),
+            callbacks=[tb_callback]
+        )
+        path_out = path_weights / str(val_fold) / "ethnicity_fc_model.h5"
+        path_out.parent.mkdir(exist_ok=True, parents=True)
+        model.save_weights(str(path_out))
 
-    model.fit(
-        train_features,
-        train_labels,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(val_features, val_labels),
-        callbacks=[tb_callback]
-    )
-    model.save_weights(str(path_weights / "ethnicity_fc_model.h5"))
-    plot_summary(epochs, f"{args.logs}/ethnicity/{output_tag}plot_summary.pdf")
+        f_out = f"{args.logs}/ethnicity/{output_tag}/{val_fold}/plot_summary"
+        plot_summary(epochs, f"{f_out}.pdf")
